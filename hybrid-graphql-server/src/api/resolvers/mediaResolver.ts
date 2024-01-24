14ac49b6e9c158c4193b8abf3e5ad8818d5bc832
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-01-24 14:17:54 +0200

    Schemas

--- hybrid-graphql-server/src/api/resolvers/mediaResolver.ts
+++ hybrid-graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,9 +1,21 @@
-import {fetchAllMedia} from '../models/mediaModel';
+import {MediaItem} from '@sharedTypes/DBTypes';
+import {
+  fetchAllMedia,
+  fetchMediaById,
+  fetchMediaByTag,
+} from '../models/mediaModel';
 
 export default {
   Query: {
     mediaItems: async () => {
       return await fetchAllMedia();
+    },
+    mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+      const id = Number(args.media_id);
+      return await fetchMediaById(id);
+    },
+    mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
+      return await fetchMediaByTag(args.tag);
     },
   },
 };
---
+++ hybrid-graphql-server/src/api/resolvers/tagResolver.ts
@@ -0,0 +1,16 @@
+import {MediaItem} from '@sharedTypes/DBTypes';
+import {fetchAllTags, fetchTagsByMediaId} from '../models/tagModel';
+
+export default {
+  MediaItem: {
+    tags: async (parent: {media_id: string}) => {
+      console.log(parent);
+      return await fetchTagsByMediaId(Number(parent.media_id));
+    },
+  },
+  Query: {
+    tags: async () => {
+      return await fetchAllTags();
+    },
+  },
+};
